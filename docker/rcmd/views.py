849b19235bfe0fbf6bac1a52aeb7ca30ac1a26d2
bonzoyang <bonzoyang@gmail.com>
2020-10-02 00:45:45 +0800

    rm __pycache__, add rx() to rcmd/views.py

--- docker/rcmd/views.py
+++ docker/rcmd/views.py
@@ -15,6 +15,28 @@ from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
+
+def rx(rcvj, key='view'):
+    '''
+    receiver decoder
+    rcvj: received json
+    key: onee of 'view', 'select', 'blog'
+
+    rcvj[key]:
+        [{'id': 'C179002853-ORNL_DAAC', 'time': '2020-10-01 20:33:45'}, 
+        {'id': 'C179003246-ORNL_DAAC', 'time': '2020-09-30 05:44:45'}, 
+        {'id': 'C1251051178-GES_DISC', 'time': '2020-09-30 04:38:45'}]
+    '''
+    ids = []
+    times = []
+    for e in rcvj[key]:
+        ids.append(e['id'])
+        times.append(e['time'])
+
+    return ids, times
+    
+
+
 @csrf_exempt
 def recommend(request):
     print(type(request.body))
@@ -24,13 +46,16 @@ def recommend(request):
     s = rcv['select']
     b = rcv['blog']
 
+    vids, vtimes = rx(rcv, 'view')
+    sids, stimes = rx(rcv, 'select')
+    bids, btimes = rx(rcv, 'blog')
+
+    
+
 
     # fake recommend data
-    rr = ['C179002853-ORNL_DAAC', 'C179003246-ORNL_DAAC', 'C1251051178-GES_DISC',
-    'C1577578302-SEDAC', 'C1000001080-LARC_ASDC', 'C1674784946-PODAAC', 'C1237113469-GES_DISC',
-    'C1235316217-GES_DISC', 'C1276812927-GES_DISC', 'C1274767826-GES_DISC', 'C1274764823-GES_DISC', 'C191032907-LARC',
-    'C179124085-ORNL_DAAC', 'C1906827740-ORNL_DAAC', 'C1214474243-ASF', 'C7611230-LARC_ASDC',
-    'C1684267683-PODAAC', 'C1597990346-NOAA_NCEI', 'C1625128618-GHRC_CLOUD', 'C1898287521-GHRC_CLOUD']
+    rr = vids+sids+bids
+        
     res = '['
     for r in rr:
         with open(f'{settings.BASE_DIR}/rcmd/json/{r}.json', 'r') as f:
@@ -48,13 +73,16 @@ def search(request):
     s = rcv['select']
     b = rcv['blog']
 
+    vids, vtimes = rx(rcv, 'view')
+    sids, stimes = rx(rcv, 'select')
+    bids, btimes = rx(rcv, 'blog')
+
+    
+
 
     # fake recommend data
-    rr = ['C179002853-ORNL_DAAC', 'C179003246-ORNL_DAAC', 'C1251051178-GES_DISC',
-    'C1577578302-SEDAC', 'C1000001080-LARC_ASDC', 'C1674784946-PODAAC', 'C1237113469-GES_DISC',
-    'C1235316217-GES_DISC', 'C1276812927-GES_DISC', 'C1274767826-GES_DISC', 'C1274764823-GES_DISC', 'C191032907-LARC',
-    'C179124085-ORNL_DAAC', 'C1906827740-ORNL_DAAC', 'C1214474243-ASF', 'C7611230-LARC_ASDC',
-    'C1684267683-PODAAC', 'C1597990346-NOAA_NCEI', 'C1625128618-GHRC_CLOUD', 'C1898287521-GHRC_CLOUD']
+    rr = vids+sids+bids
+        
     res = '['
     for r in rr:
         with open(f'{settings.BASE_DIR}/rcmd/json/{r}.json', 'r') as f:
